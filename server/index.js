04963530f2a2e988cb95a206ce71f9ce38b8f8ea
ontariodf <robin@onlinedivorceforms.ca>
2020-06-28 09:56:32 -0400

    push

--- server/index.js
+++ server/index.js
@@ -1,16 +1,16 @@
-"use strict";
-require("dotenv").config();
-const express = require("express");
-const parser = require("body-parser");
-const cors = require("cors");
-const sendMail = require("./send_mail").sendMail;
+'use strict';
+require('dotenv').config();
+const express = require('express');
+const parser = require('body-parser');
+const cors = require('cors');
+const sendMail = require('./send_mail').sendMail;
 
 const app = express();
 
 app.use(parser.json());
 app.use(cors());
 
-app.post("/sendmail", sendMail);
+app.post('/sendmail', sendMail);
 
 app.listen(8888, () => {
   console.info(`Listening on 8888`);
--- server/send_mail.js
+++ server/send_mail.js
@@ -1,19 +1,19 @@
-"use strict";
-const nodemailer = require("nodemailer");
+'use strict';
+const nodemailer = require('nodemailer');
 
 module.exports.sendMail = async (req, res) => {
   const { name, email, message, recaptchaValue } = req.body.info;
 
   const smtpConfig = {
-    service: "gmail",
+    service: 'gmail',
     auth: {
       user: process.env.ADMIN_EMAIL,
-      pass: process.env.MAILPASS
-    }
+      pass: process.env.MAILPASS,
+    },
   };
 
   if (!recaptchaValue) {
-    return res.send({ error: "Please check the captcha" });
+    return res.send({ error: 'Please check the captcha' });
   }
 
   const transporter = nodemailer.createTransport(smtpConfig);
@@ -21,18 +21,18 @@ module.exports.sendMail = async (req, res) => {
   try {
     await transporter.sendMail({
       to: process.env.DEV_EMAIL,
-      subject: "Incubo Development Inquiry",
+      subject: 'Incubo Development Inquiry',
       html: `
       <p>From: ${name} - ${email}</p>
       <p>${message}</p>
-    `
+    `,
     });
 
     res.send({ ok: "Thanks got it! I'll be in touch." });
   } catch (err) {
     console.error(err.message);
     res.send({
-      error: "There was a problem sending your email. Please try again later."
+      error: 'There was a problem sending your email. Please try again later.',
     });
   }
-}
+};
